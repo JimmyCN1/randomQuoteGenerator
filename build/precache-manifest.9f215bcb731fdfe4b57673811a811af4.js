f5e590ccf4899a998cd0888334a42eca8ccbe227
JimmyCN1 <chibueze.e.njoku@gmail.com>
2019-07-06 22:45:37 +1000

    url change

---
+++ build/precache-manifest.9f215bcb731fdfe4b57673811a811af4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d5679b76b39e6a98ef63e204f92e2f",
+    "url": "/randomQuoteMachine/index.html"
+  },
+  {
+    "revision": "f56838b249c031cb9902",
+    "url": "/randomQuoteMachine/static/css/main.cd7c5c4d.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/randomQuoteMachine/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "f56838b249c031cb9902",
+    "url": "/randomQuoteMachine/static/js/main.c30a252c.chunk.js"
+  },
+  {
+    "revision": "80d274e5e3a98e470765",
+    "url": "/randomQuoteMachine/static/js/runtime~main.bc825573.js"
+  }
+]);
