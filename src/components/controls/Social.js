663a2c527f76e48a38fee74a3baaf0d9a3500284
JimmyCN1 <chibueze.e.njoku@gmail.com>
2019-07-06 22:40:45 +1000

    added dummy links for the tumblr + facebook icons

--- src/components/controls/Social.js
+++ src/components/controls/Social.js
@@ -1,25 +1,42 @@
 import React from "react";
 
-function Social(props) {
-  return (
-    <div>
-      <a
-        // class="twitter-share-button"
-        href={`https://twitter.com/intent/tweet?text=${props.quote}`}
-      >
-        <i className="fab fa-twitter-square fa-3x" style={styles} />
-      </a>
+import AppTheme from "../../AppTheme.js";
 
-      <i className="fab fa-tumblr-square fa-3x" style={styles} />
-      <i className="fab fa-facebook-square fa-3x" style={styles} />
-    </div>
-  );
+class Social extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static contextType = AppTheme;
+
+  render() {
+    const styles = {
+      padding: "0px 3px",
+      margin: "0 !important",
+      alignSelf: "flex-end",
+      textDecoration: "none"
+    };
+
+    styles.color = this.context.theme;
+
+    return (
+      <div>
+        <a
+          style={styles}
+          href={`https://twitter.com/intent/tweet?text=${this.props.quote}`}
+        >
+          <i className="fab fa-twitter-square fa-3x" />
+        </a>
+
+        <a style={styles} href="#">
+          <i className="fab fa-tumblr-square fa-3x" />
+        </a>
+        <a style={styles} href="#">
+          <i className="fab fa-facebook-square fa-3x" />
+        </a>
+      </div>
+    );
+  }
 }
-
-const styles = {
-  padding: "0px 3px",
-  margin: "0 !important",
-  alignSelf: "flex-end"
-};
 
 export default Social;
